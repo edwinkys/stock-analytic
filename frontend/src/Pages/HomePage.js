dee20a97a13a5c116beb3913da4c7dff5e4f8520
Edwin Kys <itsedwinkys@gmail.com>
2021-03-04 22:58:06 -0700

    Add Function

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -16,14 +16,19 @@ import ToPercentage from "../Lib/ToPercentage";
 const HomePage = () => {
   const [isDataLoaded, setDataLoaded] = useState(false);
   const [spyData, setSpyData] = useState({});
+  const [vooData, setVooData] = useState({});
+  const [iwnData, setIwnData] = useState({});
+  const [qqqData, setQqqData] = useState({});
   const history = useHistory();
 
   const activePeriod = '1m';
-  let increasingSpyStyle;
+  let increasingSpyStyle, increasingVooStyle, increasingIwnStyle, increasingQqqStyle;
 
-  // Gather SPY Data
+  // Gather Data
   useEffect(() => {
     setDataLoaded(false);
+
+    // Gather SPY data
     Axios.get("/data", {
       params: {
         ticker: 'spy',
@@ -32,6 +37,51 @@ const HomePage = () => {
     })
       .then((response) => {
         setSpyData(response.data);
+      })
+      .catch((error) => {
+        history.push("/page-not-found");
+        console.log(error);
+      });
+
+    // Gather VOO data
+    Axios.get("/data", {
+      params: {
+        ticker: 'voo',
+        period: activePeriod
+      }
+    })
+      .then((response) => {
+        setVooData(response.data);
+      })
+      .catch((error) => {
+        history.push("/page-not-found");
+        console.log(error);
+      });
+
+    // Gather IWN data
+    Axios.get("/data", {
+      params: {
+        ticker: 'iwn',
+        period: activePeriod
+      }
+    })
+      .then((response) => {
+        setIwnData(response.data);
+      })
+      .catch((error) => {
+        history.push("/page-not-found");
+        console.log(error);
+      });
+
+    // Gather QQQ data
+    Axios.get("/data", {
+      params: {
+        ticker: 'qqq',
+        period: activePeriod
+      }
+    })
+      .then((response) => {
+        setQqqData(response.data);
         setDataLoaded(true);
       })
       .catch((error) => {
@@ -40,60 +90,80 @@ const HomePage = () => {
       });
   }, [history]);
 
-  // Check if the SPY is increasing
-  if (spyData.isIncreasing) {
-    increasingSpyStyle = " text-secondary";
-  }
-  else {
-    increasingSpyStyle = " text-error";
-  }
+  // Check if the ETF is increasing
+  increasingSpyStyle = spyData.isIncreasing ? " text-secondary" : " text-error";
+  increasingVooStyle = vooData.isIncreasing ? " text-secondary" : " text-error";
+  increasingIwnStyle = iwnData.isIncreasing ? " text-secondary" : " text-error";
+  increasingQqqStyle = qqqData.isIncreasing ? " text-secondary" : " text-error";
+
+  // ETF Data
+  const etfData = [
+    {
+      title: "SPY",
+      data: spyData,
+      style: increasingSpyStyle
+    },
+    {
+      title: "VOO",
+      data: vooData,
+      style: increasingVooStyle
+    },
+    {
+      title: "IWN",
+      data: iwnData,
+      style: increasingIwnStyle
+    },
+    {
+      title: "QQQ",
+      data: qqqData,
+      style: increasingQqqStyle
+    }
+  ];
 
   return (
     <DefaultLayout isLoaded={isDataLoaded}>
       <div className="container wrapper">
         <div className="section-sm flex-col">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
-            <Card title="SPY">
-              <span className={"mb-3" + increasingSpyStyle}>
-                {
-                  spyData.latestPrice ?
-                  FixedDecimal(spyData.latestPrice) :
-                  "-"
-                }
-              </span>
-              <span className={"text-xs mb-6" + increasingSpyStyle}>
-                (
-                {
-                  spyData.isIncreasing ?
-                  "+" :
-                  null
-                }
-                {
-                  spyData.increment ?
-                  FixedDecimal(spyData.increment) :
-                  null
-                }
-                &emsp;
-                {
-                  spyData.isIncreasing ?
-                  "+" :
-                  null
-                }
-                {
-                  spyData.incrementPercentage ?
-                  ToPercentage(spyData.incrementPercentage) :
-                  null
-                }
-                )
-              </span>
-              <StockChart data={spyData.price} label="Time" value="Close" trend="Close Prediction" average="Close Mean" isIncreasing={spyData.isIncreasing} height={150} />
-            </Card>
-            <Card title="VOO">
-            </Card>
-            <Card title="QQQ">
-            </Card>
-            <Card title="IWM">
-            </Card>
+            {
+              etfData.map((item, i) => (
+                <Card key={i} title={item.title}>
+                  <span className={"mb-3" + item.style}>
+                    {
+                      item.data.latestPrice ?
+                      FixedDecimal(item.data.latestPrice) :
+                      "-"
+                    }
+                  </span>
+                  <span className={"text-xs mb-6" + item.style}>
+                    (
+                    {
+                      item.data.isIncreasing ?
+                      "+" :
+                      null
+                    }
+                    {
+                      item.data.increment ?
+                      FixedDecimal(item.data.increment) :
+                      null
+                    }
+                    &emsp;
+                    {
+                      item.data.isIncreasing ?
+                      "+" :
+                      null
+                    }
+                    {
+                      item.data.incrementPercentage ?
+                      ToPercentage(item.data.incrementPercentage) :
+                      null
+                    }
+                    )
+                  </span>
+                  <StockChart data={item.data.price} label="Time" value="Close" trend="Close Prediction" average="Close Mean" isIncreasing={item.data.isIncreasing} height={150} />
+                </Card>
+              ))
+            }
           </div>
         </div>
       </div>
